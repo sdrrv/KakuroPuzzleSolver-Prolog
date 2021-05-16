f43e1ebc756b731b7274fd2a99a75f105aac947b
sdrrv <duartecelvas@gmail.com>
2021-05-16 23:49:59 +0100

    Fix?

--- main.pl
+++ main.pl
@@ -369,15 +369,18 @@ experimenta_perm([Esp,Lst_Perms], Perms_Possiveis,Novas_Perms_Possiveis):-
     replace_permPoss(Perms_Possiveis,[Esp,Lst_Perms],El,Novas_Perms_Possiveis).
 
 %--------------------------------------------------------------------------------------
-
+%
 resolve_aux(Perms_Possiveis, Novas_Perms_Possiveis):-
-    resolve_aux2(Perms_Possiveis, Novas_Perms_Possiveis).
+    length(Perms_Possiveis,Len),
+    resolve_aux(Perms_Possiveis, Novas_Perms_Possiveis,Len).
     
-resolve_aux2(Perms_Possiveis, Novas_Perms_Possiveis):-
+resolve_aux(Perms_Possiveis, Novas_Perms_Possiveis,Len):-
     escolhe_menos_alternativas(Perms_Possiveis, Escolha),!,
 	experimenta_perm(Escolha, Perms_Possiveis, Res01),
     simplifica(Res01,Res02),
-    resolve_aux2(Res02, Novas_Perms_Possiveis).
+    resolve_aux(Res02, Novas_Perms_Possiveis,Len).
     
-resolve_aux2(Perms_Possiveis, Perms_Possiveis):-
+resolve_aux(Perms_Possiveis, Perms_Possiveis,Len):-
+    length(Perms_Possiveis,ResLen),
+    ResLen == Len,
     !.
